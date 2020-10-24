dcb51ecad4a4a72d4c19c3b9d2c779cc979b7a4a
HonusDaniel <danielhonus@outlook.cz>
2020-10-24 22:31:50 +0200

    patches

--- config.h
+++ config.h
@@ -2,17 +2,21 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappx     = 6;
+static const unsigned int gappx     = 20;
 static const int showbar            = 1;        /* 0 means no bar */
+static const int vertpad            = 0;       /* vertical padding of bar */
+static const int sidepad            = 0;       /* horizontal padding of bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "source_code_variable:size=10" };
+static const char dmenufont[]       = "source_code_variable:size=11";
+static const int user_bh            = 20;        /* 0 means that dwm will calculate bar height, >= 1 means dwm     will user_bh as bar height */
+
 //background color
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#1e1e1e";
 //inactive window border color
 static const char col_gray2[]       = "#444444";
 //font
-static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray3[]       = "#ffffff";
 //tag color
 static const char col_gray4[]       = "#eeeeee";
 //top bar second color (blue) and active window border
@@ -23,8 +27,9 @@ static const char *voldowncmd[]  = { "mpc", "volume", "-1", NULL };
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+  [SchemeTitle]  = { col_cyan, col_gray1,  col_cyan  },
 };
 
 /* tagging */
@@ -67,10 +72,12 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "gnome-terminal", NULL };
+static const char *firefox[]  = {"firefox", NULL};
 #include "shiftview.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+  { MODKEY|ShiftMask,             XK_f,      spawn,          {.v = firefox  } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -95,6 +102,9 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
   { MODKEY,                       XK_e,      shiftview,      {.i = +1 } },
   { MODKEY,                       XK_w,      shiftview,      {.i = -1 } },
+  { MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
   { 0,                            0x1008ff13, spawn,         {.v = volupcmd } },
   { 0,                            0x1008ff11, spawn,         {.v = voldowncmd } },
 	TAGKEYS(                        XK_1,                      0)
